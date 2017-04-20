c4dcf8d4626457e8f527a98be702039bf64decef
shturnev <sht_job@ukr.net>
2017-04-21 01:14:22 +0300

    + аниммация переходим к малому экрану

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@
  * Created by sht-home on 20.04.2017.
  */
 
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.css({opacity: 1}).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+
 /*-----------------------------------
 Расставим точки
 -----------------------------------*/
@@ -17,10 +26,52 @@ function setDotsPosition(n) {
         var t = $(".items > .rou:eq("+ i +")").position().top;
 
         //Установим размеры для dots
-        var t2 = $(".red-dot > .cont:eq("+ i +")").css({top: t + "px"});
-        console.log(t2);
-    }    
+        $(".red-dot > .cont:eq("+ i +")").css({top: t + "px"});
+    }
     
 }
 
+
+/*-----------------------------------
+Анимации
+-----------------------------------*/
+function my_animate(style) {
+
+    var elm = $('[data-js-animate="'+ style +'"]'),
+        stopClass = 'STOP-EFFECT';
+
+    $(elm).each(function () {
+
+        if($(this).is(":in-viewport") && !$(this).hasClass(stopClass))
+        {
+            $(this).addClass(stopClass);
+            $(this).animateCss(style);
+        }
+
+    });
+
+
+}
+
+
+
+
+
+
+
+
+
+
+/*-----------------------------------
+INIT
+-----------------------------------*/
 setDotsPosition($(".red-dot > .cont").length - 1);
+
+$(document).scroll(function () {
+    my_animate('bounceInUp');
+    my_animate('fadeIn');
+    my_animate('bounce');
+    my_animate('bounceInLeft');
+    my_animate('bounceInRight');
+    my_animate('fadeInUp');
+});
